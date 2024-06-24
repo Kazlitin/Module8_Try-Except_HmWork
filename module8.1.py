2bb5c1f993f984aa49b82f0c9fe46613b876be3d
Kazlitin <167462725+Kazlitin@users.noreply.github.com>
2024-06-24 20:42:37 +0300

    Add files via upload

--- module8.1.py
+++ module8.1.py
@@ -1,7 +1,7 @@
 def add_everything_up(a, b):
     try:
         return a + b
-    except TypeError as e:
+    except TypeError as error:
         # Если возникла ошибка типа, значит a и b разных типов
         # Возвращаю строковое представление этих двух данных вместе
         return f"{a} {b}"
